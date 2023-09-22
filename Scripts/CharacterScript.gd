d82667546a1c1e3ccb5249f941bfa4a2f1b070aa
Ahmed Ghaleb <58638184+7azmi@users.noreply.github.com>
2023-09-22 08:41:13 +0800

    half implemented movement

--- Scripts/CharacterScript.gd
+++ Scripts/CharacterScript.gd
@@ -1,29 +1,22 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	
+	var motion = Vector2()
+	if Input.is_action_pressed("left"):
+		motion.x = -1
+		
+	if Input.is_action_pressed("right"):
+		motion.x = 1
+	if Input.is_action_pressed("up"):
+		motion.y = 1
+	if Input.is_action_pressed("down"):
+		motion.y = -1
+	
+	motion = motion.normalized()
+	#velocity = motion * SPEED
+	print(motion)
+	#look_at(get_global_mouse_position())
 	
